6580298d664553c5872f1223b70331f26274a8fe
Igor Souza <igor.souza.webmaster@gmail.com>
2023-04-21 16:19:37 -0300

    feat: add new props to entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/value-objects/unique-entity-id";
 
 type AnswerProps = {
+  authorId: UniqueEntityID;
+  questionId: UniqueEntityID;
   content: string;
-  authorId: string;
-  questionId: string;
+  createdAt: Date;
+  updatedAt?: Date;
 };
 
 export class Answer extends Entity<AnswerProps> {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,16 @@
 import { Slug } from "./value-objects/slug";
+
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/value-objects/unique-entity-id";
 
 type QuestionProps = {
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
   title: string;
   content: string;
   slug: Slug;
-  authorId: string;
+  createdAt: Date;
+  updatedAt?: Date;
 };
 
 export class Question extends Entity<QuestionProps> {}
